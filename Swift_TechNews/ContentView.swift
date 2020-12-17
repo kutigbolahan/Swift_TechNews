62598dd15ae60669ccf9f26e23f5a06f74fddba0
kuti gbolahan <kutigbolahan.ab@gmail.com>
2020-12-17 13:04:12 -0800

    Initial Commit

---
+++ Swift_TechNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift_TechNews
+//
+//  Created by Mac on 12/17/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
